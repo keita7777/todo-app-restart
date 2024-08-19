9283b975520f143a5fadee8b66c365e2d982e6ef
github keita7777 <github mine0kei@gmail.com>
2024-08-19 19:30:54 +0900

    🐛 authOptionsを別ファイルで定義しインポート

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,5 @@
+import { authOptions } from "@/app/lib/next-auth/options";
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-
-export const authOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID ?? "",
-      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
-    }),
-  ],
-};
 
 const handler = NextAuth(authOptions);
 
