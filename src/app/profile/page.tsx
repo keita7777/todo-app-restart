21a2760890bbda49b7369b6fe84e65fef9d2933b
github keita7777 <github mine0kei@gmail.com>
2024-08-20 19:57:39 +0900

    ✨ プロフィールページ作成

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
-  return <div>profile</div>;
+  const { data: session } = useSession();
+  console.log(session?.user);
+
+  return (
+    <>
+      {session && (
+        <div className="bg-white px-8 py-10 flex gap-10">
+          <Image
+            src={session.user.image || "profile_icon_default.png"}
+            alt={session.user.name!}
+            width={50}
+            height={50}
+          />
+          <div>
+            <p>名前：{session?.user.name}</p>
+            <p>メールアドレス：{session?.user.email}</p>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default page;
