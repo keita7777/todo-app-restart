c4e1df2a610bb9ee2e6c7327957954a69a00fef6
github keita7777 <github mine0kei@gmail.com>
2024-08-20 20:01:43 +0900

    💄 ログイン/ログアウトページスタイル調整

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,12 +5,18 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <div className="flex flex-col">
-        <button onClick={() => signIn("github", { callbackUrl: "/todos" })}>
+    <div className="bg-white px-8 py-10 flex justify-center items-center">
+      <div className="flex flex-col gap-5">
+        <button
+          className="bg-gray-700 text-white p-3 rounded-md hover:bg-gray-600"
+          onClick={() => signIn("github", { callbackUrl: "/todos" })}
+        >
           githubサインイン
         </button>
-        <button onClick={() => signIn("google", { callbackUrl: "/todos" })}>
+        <button
+          className="bg-gray-700 text-white p-3 rounded-md hover:bg-gray-600"
+          onClick={() => signIn("google", { callbackUrl: "/todos" })}
+        >
           googleサインイン
         </button>
       </div>
--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -5,8 +5,11 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <button onClick={() => signOut({ callbackUrl: "/login" })}>
+    <div className="bg-white px-8 py-10 flex justify-center items-center">
+      <button
+        className="bg-gray-700 text-white p-3 rounded-md hover:bg-gray-600"
+        onClick={() => signOut({ callbackUrl: "/login" })}
+      >
         ログアウト
       </button>
     </div>
